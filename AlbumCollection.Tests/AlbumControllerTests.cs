27dff1c9ed936a3aed6dafef8d8194c454a805e0
Artie Negron <anegroncsu@gmail.com>
2019-03-08 12:33:57 -0500

    Setup for test driving a .NET Core Web App is complete.

--- AlbumCollection.Tests/AlbumControllerTests.cs
+++ AlbumCollection.Tests/AlbumControllerTests.cs
@@ -1,3 +1,5 @@
+using AlbumCollection.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -6,9 +8,14 @@ namespace AlbumCollection.Tests
     public class AlbumControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Index_Returns_A_View()
         {
+            var underTest = new AlbumController();
 
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
         }
+
     }
 }
