e6f6acd11149a0094f192646f619840124d99ffd
Artie Negron <anegroncsu@gmail.com>
2019-03-08 12:14:33 -0500

    Initial commit.

---
+++ AlbumCollection.Tests/AlbumControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AlbumCollection.Tests
+{
+    public class AlbumControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
